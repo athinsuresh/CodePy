9f2d99980015d55294c54df6e7049826678bcff7
athinsuresh <athinsuresh182@gmail.com>
2025-03-03 20:43:52 +0800

    add port

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Function to evaluate the user's code safely
 const executePythonCode = (code, callback) => {
